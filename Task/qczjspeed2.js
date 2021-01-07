c834d136e6f174e5098bdc8cabef748ad630378f
adw2s <42504839+adw2s@users.noreply.github.com>
2021-01-07 18:49:54 +0800

    Update qczjspeed2.js

--- Task/qczjspeed2.js
+++ Task/qczjspeed2.js
@@ -591,7 +591,7 @@ if (!GetUserInfourlArr[0]) {
       await addCoin();
       await addCoin2();
       await reportAss();
-      await cointowallet();
+      //await cointowallet();
       
   }
 }
